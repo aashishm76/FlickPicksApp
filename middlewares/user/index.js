021032e81baef069e53e8dd9b70acd856ea35f47
Aashish <36056138+aashishm76@users.noreply.github.com>
2020-11-22 21:34:50 -0500

    clean repo

--- middlewares/user/index.js
+++
@@ -1,6 +0,0 @@
-exports.validateEmail = require("./validateEmail");
-exports.userByEmail = require("./userByEmail")
-exports.genHashedPwd = require("./genHashedPwd");
-exports.validateRegister = require("./validateRegister");
-exports.auth = require("./auth");
-exports.validateLogin = require("./validateLogin");
--- startup/config.js
+++
@@ -1,7 +0,0 @@
-const config = require("config");
-
-module.exports = function (app) {
-  app.listen(config.get("PORT"), () => {
-    console.log("Listening on port 8080");
-  });
-};
